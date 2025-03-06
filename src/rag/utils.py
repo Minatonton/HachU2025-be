1d9f00469b388199b9931328afa4a4e366f3bae7
Ri4385 <tkd1121g@gmail.com>
2025-03-07 01:16:44 +0900

    image2textを追加

--- src/rag/utils.py
+++ src/rag/utils.py
@@ -1,7 +1,42 @@
 import base64
 
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
 
 def to_base64(image_path: str) -> str:
     """画像をbase64に変換する"""
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def get_text_from_image(image_path: str) -> str:
+    """openaiのapiでimageをtext化する"""
+
+    base64_image = to_base64(image_path)
+    client = OpenAI()
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "この画像には何が写っていますか?",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+                ],
+            }
+        ],
+    )
+
+    assert isinstance(response.choices[0].message.content, str)
+
+    return response.choices[0].message.content
