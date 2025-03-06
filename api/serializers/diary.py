d0412b611892ef801353c1ebeb0689acf1808aac
Minatonton <minatonton1207@gmail.com>
2025-03-06 16:46:36 +0900

    feat: base64 field

--- api/serializers/diary.py
+++ api/serializers/diary.py
@@ -1,9 +1,12 @@
+from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
 from api.models import Diary
 
 
 class DiaryCreateSerializer(serializers.ModelSerializer):
+    image = Base64ImageField()
+
     class Meta:
         model = Diary
         fields = ["id", "user", "content", "date", "image"]
@@ -11,6 +14,7 @@ class DiaryCreateSerializer(serializers.ModelSerializer):
 
 class DiarySerializer(serializers.ModelSerializer):
     date = serializers.DateField(read_only=True)
+    image = Base64ImageField()
 
     class Meta:
         model = Diary
