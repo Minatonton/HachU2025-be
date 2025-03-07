b2cdbbff25d5da84dcf03f5fc69c7a5e80ce42e3
Minatonton <minatonton1207@gmail.com>
2025-03-07 16:43:29 +0900

    feat: schedule model

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from api.models import Chat, Diary, DiaryTag, Section, Tag
+from api.models import Chat, Diary, DiaryTag, Schedule, Section, Tag
 
 admin.site.register(Chat)
 admin.site.register(DiaryTag)
 admin.site.register(Diary)
 admin.site.register(Section)
 admin.site.register(Tag)
+admin.site.register(Schedule)
--- api/models/__init__.py
+++ api/models/__init__.py
@@ -2,5 +2,6 @@
 from .chat import *
 from .diary import *
 from .diary_tag import *
+from .schedule import *
 from .section import *
 from .tag import *
