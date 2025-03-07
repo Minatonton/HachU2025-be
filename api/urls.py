8db322453349f684da4ad0d32902f199923a08b7
Minatonton <minatonton1207@gmail.com>
2025-03-07 16:43:58 +0900

    feat: schedule api

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import SimpleRouter
 
 from api.views.chat import ChatViewSet
 from api.views.diary import DiaryViewSet
+from api.views.schedule import ScheduleViewSet
 from api.views.section import SectionViewSet
 
 router = SimpleRouter()
@@ -12,6 +13,7 @@ app_name = "api"
 router.register("diary", DiaryViewSet, basename="diary")
 router.register("chat", ChatViewSet, basename="chat")
 router.register("section", SectionViewSet, basename="section")
+router.register("schedule", ScheduleViewSet, basename="schedule")
 
 
 urlpatterns = [
