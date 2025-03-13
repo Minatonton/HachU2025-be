ecd2a843497ce77ce73b5a094b07bf95f7825c5d
syarasyoujyu <watasi.u.1764@gmail.com>
2025-03-13 11:13:16 +0900

    画像をbase64化する際に、サイズを制限

--- src/rag/utils.py
+++ src/rag/utils.py
@@ -1,19 +1,27 @@
 import base64
+import io
 
 import requests
 from dotenv import load_dotenv
 from openai import OpenAI
+from PIL import Image
 
 load_dotenv()
 
 
-def image_url_to_base64(image_url):
-    """画像のurlをbase64に変換する"""
+def image_url_to_base64(image_url, max_width=400, max_height=200, quality=75):
     response = requests.get(image_url)
     if response.status_code == 200:
-        image_data = response.content
-        base64_encoded = base64.b64encode(image_data).decode("utf-8")
-        return base64_encoded
+        image = Image.open(io.BytesIO(response.content))
+
+        # 画像をリサイズ（長辺がmax_width or max_height以下）
+        image.thumbnail((max_width, max_height))
+
+        # 画像を圧縮してBase64に変換
+        buffered = io.BytesIO()
+        image.save(buffered, format="JPEG", quality=quality)  # JPEG形式 & 画質圧縮
+        return base64.b64encode(buffered.getvalue()).decode()
+
     raise Exception(f"Failed to fetch image, status code: {response.status_code}")
 
 
